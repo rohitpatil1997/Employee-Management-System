bd7a8cb9c8c51c9bc9a908e1a882660759a8ca31
rohitpatil1997 <rp888867@gmail.com>
2020-07-16 15:55:25 +0530

    Complete CRUD Operations

---
+++ EmployeeRegistration/Models/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeRegistration.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        [Column(TypeName ="nvarchar(250)")]
+        [Required(ErrorMessage ="This Field is Required.")]
+        [DisplayName("Full Name")]
+        public string FullName { get; set; }
+        [Column(TypeName ="varchar(10)")]
+        [DisplayName("Emp. Code")]
+        public string EmpCode { get; set; }
+        [Column(TypeName ="varchar(100)")]
+        public string Position { get; set; }
+        [Column(TypeName ="varchar(100)")]
+        [DisplayName("Office Location")]
+        public string OfficeLocation { get; set; }
+
+    }
+}
---
+++ EmployeeRegistration/Models/EmployeeContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeRegistration.Models
+{
+
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
